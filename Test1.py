eb2cd927f1c8d3618f1d22144a771ca7ff9cf855
madhu <hassan.madhu@gmail.com>
2022-08-15 15:00:23 -0500

    first commit

---
+++ Test1.py
@@ -0,0 +1,23 @@
+i=10
+print(i)
+
+b=i
+print(b)
+
+sum=i+b
+print(sum)
+
+differecne=i-b
+print(differecne)
+
+print("python \n test")
+x,y=10,20
+print(x,y,sep= '++')
+print((x==10 and y==30))
+
+name="john"
+marks=94.5
+print("name is",name,"marks are",marks)
+print("may name")
+print("names is %name
+
